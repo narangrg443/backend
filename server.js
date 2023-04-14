8a03bb207cd88a86e14efef7199e4dcdba1fc0f0
narangrg443 <narangrg48@gmail.com>
2023-04-14 15:06:12 +0545

    added styles

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// serve static files from public directory
-//app.use(express.static('public'));
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// listen for incoming socket connections
 
-app.get("/", (req, res)=> {
-  res.sendFile(__dirname+'/index.html');
-})
+// Serve index.html as the home page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+
+
+
+// Listen for incoming socket connections
 io.on('connection', function(socket) {
-  console.log('a user connected');
- 
+ // console.log('a user connected');
 
-  // listen for incoming messages from client
+  // Listen for incoming messages from client
   socket.on('message', function(data) {
-    console.log('message received: ' + data);
-    // broadcast message to all connected clients
+   // console.log('message received: ' + data);
+    // Broadcast message to all connected clients
     socket.broadcast.emit('message', data);
   });
 
-  // handle socket disconnection
+  // Handle socket disconnection
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
+  
+  
+  
+  socket.on("user-connect",username=>{
+    console.log("connects")
+    
+    const message="connected to chat";
+    socket.broadcast.emit("message",{username,message})
+  })
 });
 
-// start server
+// Start server
 const port = process.env.PORT || 3000;
 server.listen(port, function() {
   console.log(`listening on *:${port}`);
