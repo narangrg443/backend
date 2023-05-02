3401657e44885d10afe1d1ed57617cb77c99947f
narangrg443 <narangrg48@gmail.com>
2023-05-02 16:58:47 +0545

    message added in database

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const dotenv = require('dotenv').config()
 
 
 let users = 0;
+let username=null;
 // Serve static files from the public directory
 app.use(express.static("public", {
   extended: true
@@ -55,86 +56,136 @@ mongoose.connect(process.env.MONGODB_URL, {
   console.log("Failed to connect to MongoDB", err);
 });
 
-
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  receiverName: [String],
-  msgS: [String],
-  msgR: [String]
-
-});
-
-userSchema.plugin(passportLocalMongoose);
-
-const chatUser = mongoose.model('chatUser', userSchema);
-
-// Configure Passport for local authentication
-passport.use(new LocalStrategy(chatUser.authenticate()));
-passport.serializeUser(chatUser.serializeUser());
-passport.deserializeUser(chatUser.deserializeUser());
-
-
-
-
-app.get('/home', isAuthenticated, function(req, res) {
-
-  io.emit('user-login', req.user.username);
-  res.render('chat', {
-    user: req.user || {}
-  })
-});
-
-
-
-
-app.get('/register', function(req, res) {
-  res.render('register', {
-    user: req.user
-  })
-});
-
-app.get('/', function(req, res) {
-  res.render('login', {
-    message: null
-  });
-});
-
-app.get('/login',(req,res)=>{
-  res.redirect('/')
-})
-
-
-app.get('/logout', function(req, res) {
-
-
-
-
-  io.emit('user-logout', req.user.username || {});
-
-
-  req.logout(function(err) {
-    if (err) {
-      console.log(err);
-      return next(err);
+const messagesSchema = new mongoose.Schema({
+  name: String,
+  msg: String,
+  timeStamp: {
+    type: Date,
+    default: Date.now
     }
-    req.session.destroy(function(err) {
+
+  });
+
+
+  const msgSchema = new mongoose.Schema({
+    msg: [messagesSchema]
+  })
+
+
+
+  const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+
+
+
+  });
+
+
+  userSchema.plugin(passportLocalMongoose);
+
+  const chatUser = mongoose.model('chatUser', userSchema);
+  const Msg = new mongoose.model('Msg', msgSchema);
+
+  // Configure Passport for local authentication
+  passport.use(new LocalStrategy(chatUser.authenticate()));
+  passport.serializeUser(chatUser.serializeUser());
+  passport.deserializeUser(chatUser.deserializeUser());
+
+
+
+
+  app.get('/home', isAuthenticated, async function(req, res) {
+  
+
+    await Msg.findOne({})
+    .then((e)=> {
+      if(e){
+       io.emit('user-login', req.user.username);
+       res.render('chat', {
+         user: req.user || {}, msg:e||{}
+       })      
+        //console.log('msg',e)
+      }else{
+        io.emit('user-login', req.user.username);
+       res.render('chat', {
+         user: req.user || {},msg:null
+       })
+      }
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+
+    
+  });
+
+
+  /*
+
+app.get('/home', isAuthenticated, async function(req, res) {
+  try {
+    const msgObj = await Msg.findOne({});
+    io.emit('user-login', req.user.username);
+    console.log("obg",msgObj)
+    res.render('chat', {
+      user: req.user || {},
+      msg: msgObj || {}
+    });
+  } catch (err) {
+    console.log("Error fetching message object: ", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+*/
+
+
+  app.get('/register', function(req, res) {
+    res.render('register', {
+      user: req.user
+    })
+  });
+
+  app.get('/', function(req, res) {
+    res.render('login', {
+      message: null
+    });
+  });
+
+  app.get('/login', (req, res)=> {
+    res.redirect('/')
+  })
+
+
+  app.get('/logout', function(req, res) {
+
+
+
+
+    io.emit('user-logout', req.user.username || {});
+
+
+    req.logout(function(err) {
       if (err) {
         console.log(err);
         return next(err);
-      } else {
-
-        //io.emit('user-logout', req.user.username || {});
       }
+      req.session.destroy(function(err) {
+        if (err) {
+          console.log(err);
+          return next(err);
+        } else {
+
+          //io.emit('user-logout', req.user.username || {});
+        }
 
 
-      res.redirect('/');
+        res.redirect('/');
+      });
     });
   });
-});
 
-/*
+  /*
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/home',
   failureRedirect: '/'
@@ -145,108 +196,142 @@ app.post('/login', passport.authenticate('local', {
 */
 
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/home',
-  failureRedirect: '/'
+  app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/'
 
-}));
-
-
+  }));
 
 
 
 
-app.post('/register', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  chatUser.register(new chatUser({
-    username
-  }), password, (err,
-    user) => {
-    if (err) {
-      res.render('register', {
-        message: err.message
-      });
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/home');
-      });
-    }
+
+  app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    chatUser.register(new chatUser({
+      username
+    }), password, (err,
+      user) => {
+      if (err) {
+        res.render('register', {
+          message: err.message
+        });
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/home');
+        });
+      }
+    });
   });
-});
 
 
-app.post('/', (req, res)=> {
-  res.render('login',
-    {
-      message: "login fail"
-    })
-})
+  app.post('/', (req, res)=> {
+    res.render('login',
+      {
+        message: "login fail"
+      })
+  })
 
 
-app.post("/home", isAuthenticated, (req, res) => {
-  //save messages to mongoose
+  app.post("/home", isAuthenticated, (req, res) => {
+    //save messages to mongoose
 
-  // Redirect to home.html with username in the query string
-  res.redirect('/home');
-});
-
-
+    // Redirect to home.html with username in the query string
+    res.redirect('/home');
+  });
 
 
 
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    // If user is authenticated, call next to continue with the request
-    return next();
+
+
+  function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      // If user is authenticated, call next to continue with the request
+      return next();
+    }
+
+    // If user is not authenticated, redirect to login page
+    res.redirect('/login');
   }
 
-  // If user is not authenticated, redirect to login page
-  res.redirect('/login');
-}
 
 
 
 
 
 
-
-// Listen for incoming socket connections
-io.on('connection', function(socket) {
-
+  // Listen for incoming socket connections
+  io.on('connection', function(socket) {
 
 
-  // Listen for incoming messages from client
 
-  socket.on('message',
-    function(data) {
-      // console.log('message received: ' + data);
-      // Broadcast message to all connected clients
-      socket.broadcast.emit('message', data);
+    // Listen for incoming messages from client
+
+    socket.on('message',
+      function({
+        username, message
+      }) {
+
+        data = {
+          username, message
+        };
+        // assuming you have imported and defined the Msg model and message object somewhere above
+
+        Msg.findOne({}) // find any document in the Msg collection
+        .then((foundMsg) => {
+          if (foundMsg) {
+            // if document exists
+            foundMsg.msg.push({
+              name: username, msg: message
+            }); // add new message object to the existing msg array
+            return foundMsg.save(); // update and save the document
+          } else {
+            // if document doesn't exist
+            const newMsg = new Msg({
+              msg: [{
+                name: username, msg: message
+              }]}); // create a new document with the message object
+            return newMsg.save(); // save the new document
+          }
+        })
+        .then((savedMsg) => {
+         // console.log('Message saved:', savedMsg); // log the saved or updated document
+        })
+        .catch((error) => {
+          console.error('Error saving message:', error); // log any errors
+        });
+
+
+        socket.broadcast.emit('message',
+          data);
+
+
+      });
+
+    // Handle socket disconnection
+
+
+    socket.on('disconnect', function() {
+
+      console.log('user disconnected');
     });
 
-  // Handle socket disconnection
 
 
-  socket.on('disconnect', function() {
 
-    console.log('user disconnected');
+
+
   });
 
 
 
 
 
-
-});
-
-
-
-
-
-// Start server
-const port = process.env.PORT || 3000;
-server.listen(port, function() {
-  console.log(`listening on *:${port}`);
-});
+  // Start server
+  const port = process.env.PORT || 3000;
+  server.listen(port, function() {
+    console.log(`listening on *:${port}`);
+  });
